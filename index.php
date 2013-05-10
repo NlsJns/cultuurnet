7dc8bae22e37f7f1f5ae39fe3f2660de30a6c8b4
Nils Janssens <janssens.nils@gmail.com>
2013-05-10 22:14:02 +0200

    basis steden werkt

--- index.php
+++ index.php
@@ -1,4 +1,20 @@
 <?php
+	if(isset($_POST['Steden'])) {
+		$city = $_POST['Steden'];
+		$url = "http://build.uitdatabank.be/api/events/search?key=AEBA59E1-F80E-4EE2-AE7E-CEDD6A589CA9&city=" . $city . "&format=json";
+		$events = json_decode(file_get_contents($url));
+		$eventen = "";
+		foreach($events as $e)
+		{
+			$eventen = "test";
+		}
+		if($eventen == "test") {
+			header('Location: evenementen.php?city=' . $city );
+		}
+		else {
+			echo("Sorry, er zijn geen evenementen gevonden.");
+		}
+	}
 ?>
 <!DOCTYPE html>
 <!--[if IE 8]>    <html class="no-js lt-ie9" lang="en"> <![endif]-->
@@ -18,12 +34,11 @@
 
 </head>
 <body>
-	<a href="evenementen.php?city=Antwerpen" class="citylink" data-city="Antwerpen">Antwerpen</a>
-	<a href="evenementen.php?city=Mechelen" class="citylink" data-city="Mechelen">Mechelen</a>
-	<a href="evenementen.php?city=Gent" class="citylink" data-city="Gent">Gent</a>
-	<a href="evenementen.php?city=Brussel" class="citylink" data-city="Brussel">Brussel</a>
-	<a href="evenementen.php?city=Hasselt" class="citylink" data-city="Hasselt">Hasselt</a>
-
+	<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post"><div>
+		<input type="text" name="Steden">
+        <input id="verzendknop" value="Ok!" type="submit" />
+    </form>
+    
 <!--	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
 	<script type="text/javascript" src="js/app.js"></script> -->
 
